6bef007ae3cde6f6dc4dbfb638577a94f6d20895
Elena Felder <41136058+elefeint@users.noreply.github.com>
2022-08-10 12:39:06 -0400

    chore: preserve environment on release (#429)

--- app-gradle-plugin/kokoro/release_build.sh
+++ app-gradle-plugin/kokoro/release_build.sh
@@ -5,8 +5,8 @@ set -e
 # Display commands to stderr.
 set -x
 
-sudo /opt/google-cloud-sdk/bin/gcloud components update
-sudo /opt/google-cloud-sdk/bin/gcloud components install app-engine-java
+sudo -E /opt/google-cloud-sdk/bin/gcloud components update
+sudo -E /opt/google-cloud-sdk/bin/gcloud components install app-engine-java
 
 cd github/app-gradle-plugin
 ./gradlew check prepareRelease

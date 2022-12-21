5df48baad30561425f041048f136f58d454a9f71
Mike Eltsufin <meltsufin@google.com>
2022-12-21 16:33:30 -0500

    ci: fix GCP_UBUNTU_DOCKER Kokoro build (#909)

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -9,4 +9,16 @@ sudo /opt/google-cloud-sdk/bin/gcloud components update
 sudo /opt/google-cloud-sdk/bin/gcloud components install app-engine-java
 
 cd github/appengine-plugins-core
-./mvnw clean install cobertura:cobertura -B -U
+
+unset JAVA_TOOL_OPTIONS
+update-java-alternatives -s /usr/lib/jvm/java-1.8.0-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+
+if [ "$EUID" -ne 0 ]
+then
+  # not running as root
+  ./mvnw clean install cobertura:cobertura -B -U
+else
+  # running as root - skip file permissions tests that don't work on Docker
+  ./mvnw clean install cobertura:cobertura -B -U -Dtest=!FilePermissionsTest
+fi

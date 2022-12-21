4e765c394d9f9ce605b8884be9dfc3d97f3156b0
Mike Eltsufin <meltsufin@google.com>
2022-12-21 10:38:47 -0500

    ci: fix Kokoro build on macsvc-monterey (#907)
    * ci: fix Kokoro build on macsvc-monterey
    
    * Update continuous_mac.sh

--- appengine-plugins-core/kokoro/continuous_mac.sh
+++ appengine-plugins-core/kokoro/continuous_mac.sh
@@ -5,6 +5,8 @@ set -e
 # Display commands to stderr.
 set -x
 
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-8-latest/Contents/Home
+
 gcloud components update
 gcloud components install app-engine-java
 

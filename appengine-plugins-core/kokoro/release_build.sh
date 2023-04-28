bf12c001a0038613d1962ec93fc08775a77aeb3e
Lawrence Qiu <lawrenceqiu@google.com>
2023-04-28 16:26:56 +0000

    chore: Enable quiet mode for release script (#921)

--- appengine-plugins-core/kokoro/release_build.sh
+++ appengine-plugins-core/kokoro/release_build.sh
@@ -5,8 +5,8 @@ set -e
 # Display commands to stderr.
 set -x
 
-sudo -E /opt/google-cloud-sdk/bin/gcloud components update
-sudo -E /opt/google-cloud-sdk/bin/gcloud components install app-engine-java
+sudo -E /opt/google-cloud-sdk/bin/gcloud components update -q
+sudo -E /opt/google-cloud-sdk/bin/gcloud components install app-engine-java -q
 
 cd github/appengine-plugins-core
 ./mvnw -Prelease -B -U verify

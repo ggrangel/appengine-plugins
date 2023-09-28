1b4010e2506bf6fa179c6674a4acffcdb15d4112
Mike Eltsufin <meltsufin@google.com>
2023-09-28 15:28:26 -0400

    chore: add Google's Maven Central mirror (#946)

--- appengine-plugins-core/kokoro/continuous_mac.sh
+++ appengine-plugins-core/kokoro/continuous_mac.sh
@@ -11,4 +11,9 @@ gcloud components update
 gcloud components install app-engine-java
 
 cd github/appengine-plugins-core
+
+# Use GCP Maven Mirror
+mkdir -p ${HOME}/.m2
+cp settings.xml ${HOME}/.m2
+
 ./mvnw clean install cobertura:cobertura -B -U
